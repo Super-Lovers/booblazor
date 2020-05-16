79e6e0d353838b7910761eef730809e7c58e224e
Super-Lovers <n.v.ivanov@st.hanze.nl>
2020-05-16 11:27:22 +0200

    Added game config and icon

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.window.title = "Boob Lazor"
+    t.window.icon = "assets/images/game-icon.png"
+
+    t.window.width = 1024
+    t.window.height = 1024
+    t.window.resizable = false
+    -- t.window.borderless = true -- for development only
+    t.window.fullscreen  = false -- for development only
+end
